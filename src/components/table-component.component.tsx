afceee13a30f45f8fccd05536a27c68e849bc593
Lorena Anghel <anghellorena16@gmail.com>
2020-08-31 00:34:21 +0300

    resolved errors

--- src/components/table-component.component.tsx
+++ src/components/table-component.component.tsx
@@ -1,23 +1,49 @@
 import React from "react";
-import Table from "terra-table";
+import Table, { Header, HeaderCell, Body, Cell, Row } from "terra-html-table";
 
-const UserTable = () => (
-  <Table
-    summaryId="striped-table"
-    summary="This table displays striped rows."
-    numberOfColumns={3}
-    dividerStyle="horizontal"
-    headerData={{
-      cells: [
-        { id: "h-id", key: "id", children: "ID" },
-        { id: "h-username", key: "username", children: "Username" },
-        { id: "h-name", key: "name", children: "Name" },
-        { id: "h-phone", key: "phone", children: "Phone" },
-        { id: "h-mail", key: "mail", children: "Mail" },
-        { id: "h-address", key: "address", children: "Address" },
-      ],
-    }}
-    bodyData={[]}
-  />
-);
-export default UserTable;
+export default class FetchRandomUser extends React.Component {
+  state = {
+    loading: true,
+    person: null,
+  };
+
+  async componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data, loading: false });
+  }
+
+  renderTableData() {
+    return this.state.person.map((person, index) => {
+      const { id, name } = person; //destructuring
+      return (
+        <Row key={id}>
+          <Cell>{id}</Cell>
+          <Cell>{name}</Cell>
+        </Row>
+      );
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>~Didn't get a person~</div>;
+    }
+
+    return (
+      <Table>
+        <Header>
+          <HeaderCell key="NAME">Id</HeaderCell>
+          <HeaderCell key="ADDRESS">Name</HeaderCell>
+          <HeaderCell key="PHONE_NUMBER">Phone Number</HeaderCell>
+        </Header>
+        <Body>{this.renderTableData()}</Body>
+      </Table>
+    );
+  }
+}
